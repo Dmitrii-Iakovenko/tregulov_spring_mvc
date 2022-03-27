9c79d1db25d846d22a8700e16b2a92ba3df6e908
Dmitrii Iakovenko <dmitrii.iakovenko84@gmail.com>
2022-03-27 19:25:11 +0300

    62 - model form input

--- src/main/java/com/wutreg/mvc/MyController.java
+++ src/main/java/com/wutreg/mvc/MyController.java
@@ -2,39 +2,36 @@ package com.wutreg.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
+@RequestMapping("employee/")
 public class MyController {
 
-    @RequestMapping("/")
+    @RequestMapping("")
     public String showFirstView() {
         return "first-view";
     }
 
-    @RequestMapping("/askDetails")
-    public String askEmployeeDetails(){
+    @RequestMapping("askDetails")
+    public String askEmployeeDetails(Model model){
+        Employee employee = new Employee();
+        employee.setName("Иван");
+        employee.setSurname("Petrov");
+        employee.setSalary(4000);
+        model.addAttribute("employee", employee);
         return "ask-emp-details-view";
     }
 
-//    @RequestMapping("/showDetails")
-//    public String showEmpDetails(HttpServletRequest request, Model model) {
-//        String empName = request.getParameter("employeeName");
-//        empName = "Mr. " + empName;
-//        model.addAttribute("nameAttribute", empName);
-//
-//        model.addAttribute("description", " - udemy instructor");
-//
-//        return "show-employee-details-view";
-//    }
+    @RequestMapping("showDetails")
+    public String showEmpDetails(@ModelAttribute("employee") Employee employee) {
 
-    @RequestMapping("/showDetails")
-    public String showEmpDetails(@RequestParam("employeeName") String empName, Model model) {
-        empName = "Mr. " + empName + "!";
-        model.addAttribute("nameAttribute", empName);
+        String name = employee.getName();
+        employee.setName("Mr. " + name);
 
         return "show-employee-details-view";
     }
--- src/main/webapp/WEB-INF/view/ask-emp-details-view.jsp
+++ src/main/webapp/WEB-INF/view/ask-emp-details-view.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 <!DOCTYPE html>
 <html>
 <body>
@@ -6,9 +7,20 @@
 <br>
 <br>
 
-<form action="showDetails" method="get">
-    <input type="text" name="employeeName" placeholder="Write your Name"/>
-    <input type="submit"/>
-</form>
+
+<form:form action = "showDetails" modelAttribute="employee">
+    Name: <form:input path="name"/>
+    <br>
+    <br>
+    Surname: <form:input path="surname"/>
+    <br>
+    <br>
+    Salary: <form:input path="salary"/>
+    <br>
+    <br>
+    <input type="submit" value="Ok">
+</form:form>
+
+
 </body>
 </html>
