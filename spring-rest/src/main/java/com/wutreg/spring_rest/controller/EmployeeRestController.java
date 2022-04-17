31527b852d5aaa559d623924dd71bad91dba0824
Dmitrii Iakovenko <dmitrii.iakovenko84@gmail.com>
2022-04-17 11:04:20 +0300

    82 - get employee by id

--- spring-rest/src/main/java/com/wutreg/spring_rest/controller/EmployeeRestController.java
+++ spring-rest/src/main/java/com/wutreg/spring_rest/controller/EmployeeRestController.java
@@ -4,6 +4,7 @@ import com.wutreg.spring_rest.entity.Employee;
 import com.wutreg.spring_rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +24,10 @@ public class EmployeeRestController {
     @GetMapping("employees")
     public List<Employee> getAll() {
         return employeeService.getAllEmployees();
+    }
+
+    @GetMapping("employees/{id}")
+    public Employee getById(@PathVariable long id) {
+        return employeeService.getEmployee(id);
     }
 }
