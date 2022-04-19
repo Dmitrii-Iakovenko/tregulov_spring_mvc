0b973c97d5f5f4fa472d814dd425e1cda3830716
Dmitrii Iakovenko <dmitrii.iakovenko84@gmail.com>
2022-04-19 14:47:38 +0300

    90 - config spring security app

---
+++ spring_security/src/main/java/com/wutreg/spring_security/config/AppConfig.java
@@ -0,0 +1,23 @@
+package com.wutreg.spring_security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan(basePackages = "com.wutreg.spring_security")
+@EnableWebMvc
+public class AppConfig {
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver internalResourceViewResolver =
+            new InternalResourceViewResolver();
+        internalResourceViewResolver.setPrefix("/WEB-INF/view/");
+        internalResourceViewResolver.setSuffix(".jsp");
+        return internalResourceViewResolver;
+    }
+}
