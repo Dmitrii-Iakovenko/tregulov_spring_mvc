1ce06b6b73a7e08fceba657afbf82ffe6629d54b
Dmitrii Iakovenko <dmitrii.iakovenko84@gmail.com>
2022-04-19 17:03:21 +0300

    92 - add authorization

--- spring_security/src/main/java/com/wutreg/spring_security/config/SecurityConfig.java
+++ spring_security/src/main/java/com/wutreg/spring_security/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.wutreg.spring_security.config;
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -27,5 +28,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .username("ivan")
                 .password("ivan")
                 .roles("MANAGER", "HR"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .antMatchers("/").hasAnyRole("EMPLOYEE", "HR", "MANAGER")
+            .antMatchers("/hr_info").hasRole("HR")
+            .antMatchers("/manager_info").hasRole("MANAGER")
+            .and().formLogin().permitAll();
     }
 }
